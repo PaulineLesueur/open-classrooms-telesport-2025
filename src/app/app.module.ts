1816134a8374bde35dffb7d4c99154b3740fd31b
Scarounine <paulinelesueur93@gmail.com>
2025-10-10 11:23:58 +0200

    dep: uninstall ngx-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailComponent } from './pages/details/details.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { MedalsChartComponent } from "./pages/home/components/medals-chart/medals-chart.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, MedalsChartComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, DetailComponent, NgxChartsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, DetailComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/components/medals-chart/medals-chart.component.ts
+++ src/app/pages/home/components/medals-chart/medals-chart.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { NgxChartsModule } from "@swimlane/ngx-charts";
 
 @Component({
   selector: 'app-medals-chart',
@@ -9,12 +8,5 @@ import { NgxChartsModule } from "@swimlane/ngx-charts";
   styleUrl: './medals-chart.component.scss'
 })
 export class MedalsChartComponent {
-  chartData$: Observable<{ name: string, value: number }[]>;
-
-  tooltipText = (model: { name: string, value: number }) => {
-    return `${model.name}\n🏅 ${model.value}`; 
-  }
-  constructor(olympicService: OlympicService) {
-    this.chartData$ = olympicService.getCountriesChartData();
-  }
+  
 }
