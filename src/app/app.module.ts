2b2532d074942687212b4366f11ef3c97901406f
Scarounine <paulinelesueur93@gmail.com>
2025-10-10 10:26:19 +0200

    code: save before downgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailComponent } from './pages/details/details.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MedalsChartComponent } from "./pages/home/components/medals-chart/medals-chart.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule,  DetailComponent, NgxChartsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, MedalsChartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, DetailComponent, NgxChartsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -62,4 +62,12 @@ export class OlympicService {
     );
   }
 
+  getCountriesChartData(): Observable<{ name: string, value: number }[]> {
+    return this.getCountriesWithTotals().pipe(
+      map(countries => countries.map(c => ({ 
+        name: c.country, 
+        value: c.medals 
+      })))
+    );
+  }
 }
