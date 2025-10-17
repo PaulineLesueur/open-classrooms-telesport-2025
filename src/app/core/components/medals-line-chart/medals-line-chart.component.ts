1d9f996a6fdf00827f2be59725348a55100bda6b
Scarounine <paulinelesueur93@gmail.com>
2025-10-17 13:37:10 +0200

    feat: generate line chart component

---
+++ src/app/core/components/medals-line-chart/medals-line-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medals-line-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './medals-line-chart.component.html',
+  styleUrl: './medals-line-chart.component.scss'
+})
+export class MedalsLineChartComponent {
+
+}
