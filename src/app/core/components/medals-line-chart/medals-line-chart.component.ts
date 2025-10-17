66ee8f3139c687e1d2546170fa0c96a7d922c0a3
Scarounine <paulinelesueur93@gmail.com>
2025-10-17 15:29:17 +0200

    feat: line chart works

--- src/app/core/components/medals-line-chart/medals-line-chart.component.ts
+++ src/app/core/components/medals-line-chart/medals-line-chart.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
+import { map, Observable, switchMap } from 'rxjs';
+import { OlympicService } from '../../services/olympic.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-medals-line-chart',
-  standalone: true,
-  imports: [],
   templateUrl: './medals-line-chart.component.html',
   styleUrl: './medals-line-chart.component.scss'
 })
-export class MedalsLineChartComponent {
+export class MedalsLineChartComponent implements OnInit {
+  data$!: Observable<ChartData>;
+  options!: ChartOptions<'line'>;
+  countryName!: string;
+  lineColor = '#04838F'; 
 
+  private colors = ['#956065', '#b8cbe7', '#89a1db', '#793d52', '#9780a1']; 
+
+  constructor(private olympicService: OlympicService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const countryId$ = this.route.paramMap.pipe(
+      map(params => Number(params.get('id')))
+    );
+
+    this.data$ = countryId$.pipe(
+      switchMap(id => this.olympicService.getCountryParticipations(id).pipe(
+        map(participations => {
+          if (!participations) return { labels: [], datasets: [] } as ChartData;
+
+          participations.sort((a, b) => a.year - b.year);
+
+          this.olympicService.getOlympics().subscribe(olympics => {
+            const country = olympics?.find(c => c.id === id);
+            this.countryName = country?.country ?? '';
+          });
+
+          const index = id - 1; 
+          this.lineColor = this.colors[index % this.colors.length];
+
+          return {
+            labels: participations.map(p => p.year.toString()),
+            datasets: [
+              {
+                data: participations.map(p => p.medalsCount),
+                borderColor: this.lineColor,
+                borderWidth: 2,
+                pointRadius: 2, 
+                backgroundColor: [this.lineColor],
+                hoverBackgroundColor: [this.lineColor]
+              }
+            ]
+          } as ChartData;
+        })
+      ))
+    );
+
+    this.options = {
+      plugins: {
+        legend: { display: false },
+        tooltip: { 
+          displayColors: false,
+          backgroundColor: '#04838F',
+          bodyColor: '#FFFFFF',
+          borderColor: '#04838F',
+          padding: { top: 5, bottom: 5, left: 15, right: 15 },
+          yAlign: 'bottom',
+          titleAlign: 'center',
+          titleFont: { size: 16, weight: 400, family: 'Plus Jakarta Sans' },
+          bodyFont: { size: 16, weight: 400, family: 'Plus Jakarta Sans' },
+          bodyAlign: 'center',
+          footerAlign: 'center',
+          caretSize: 0,
+          callbacks: {
+            label: (context) => `🏅 ${context.parsed.y}`,
+            title: () => ''
+          }
+         }
+      },
+      scales: {
+        x: { title: { display: true, text: 'Dates' } },
+        y: { title: { display: true, text: 'Médailles' }, beginAtZero: true }
+      }
+    };
+  }
 }
